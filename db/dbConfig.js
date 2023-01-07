9a361b328bb61b1dd915a9f2d9cb3c4dd13b3f0f
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2023-01-07 21:35:51 +0100

    breaking down

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,6 +2,12 @@ import mysql from "mysql";
 import env from "../src/services/env.js";
 env();
 
+console.log({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_SERVER,
+});
 const conn = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -10,7 +16,14 @@ const conn = mysql.createConnection({
 });
 
 conn.connect(function (err) {
-  if (err) throw err;
+  ///////>>> this is where the deployment crashes
+  if (err)
+    throw err.message({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_SERVER,
+    });
   console.log("database connected");
 });
 
